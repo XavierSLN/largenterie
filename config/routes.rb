963565a4ad5597397888b4f6a9e0a4c97a62e962
XavierSLN <xav.sln@gmail.com>
2016-08-23 12:03:33 +0200

    added pages bijoux, laboutique et contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  get 'static_pages/bijoux'
+
+  get 'static_pages/laboutique'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#index'
+
+  get 'static_pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
